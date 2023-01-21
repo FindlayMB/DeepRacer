0c1e725806da572a03a9007452d207757f6bf5ec
ParsaKargari <parsakargari@ucalgary.ca>
2023-01-20 22:36:45 -0700

    brushed up (first test)

--- model-p.py
+++ model-p.py
@@ -1,43 +1,44 @@
 def reward_function(params):
     
+    #Follow the center line
+
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
     all_wheels_on_track = params['all_wheels_on_track']
     car_speed = params['speed']
 
-    # 5 Markers on Track
+    # 5 Markers on Track / EXPANDED VERSION
 
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.20 * track_width
-    marker_3 = 0.30 * track_width
-    marker_4 = 0.40 * track_width
-    marker_5 = 0.5 * track_width
+    mark_1 = 0.1 * track_width
+    mark_2 = 0.20 * track_width
+    mark_3 = 0.30 * track_width
+    mark_4 = 0.40 * track_width
+    mark_5 = 0.5 * track_width
 
     # The more center the car, the more rewards it gets
 
-    if distance_from_center <= marker_1 and all_wheels_on_track:
+    if distance_from_center <= mark_1 and all_wheels_on_track:
         reward = 5
 
-    elif distance_from_center <= marker_2 and all_wheels_on_track:
+    elif distance_from_center <= mark_2 and all_wheels_on_track:
         reward = 4.5
 
-    elif distance_from_center <= marker_3 and all_wheels_on_track:
+    elif distance_from_center <= mark_3 and all_wheels_on_track:
         reward = 3.5
 
-    elif distance_from_center <= marker_4 and all_wheels_on_track:
+    elif distance_from_center <= mark_4 and all_wheels_on_track:
         reward = 3
 
-    elif distance_from_center <= marker_5 and all_wheels_on_track:
+    elif distance_from_center <= mark_5 and all_wheels_on_track:
         reward = 2.5
 
     else:
-
-        reward = 1e-3  # likely crashed/ close to off track
+        reward = 1e-3
 
     if car_speed < 1:
-        reward = reward + 2
+        reward += 4
 
     else:
-        reward = reward + 3
+        reward += 5
 
     return float(reward)
